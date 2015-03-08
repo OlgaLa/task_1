0576bc599b42db9b47dbdca30497d7e1518f4145
Властелин Вселенной <wartooth@tut.by>
2015-03-08 20:06:19 +0300

    create class for work with groups

---
+++ fixture/group.py
@@ -0,0 +1,117 @@
+__author__ = 'Властелин Вселенной'
+
+
+class GroupHelper:
+
+    def __init__(self, app):
+        self.app=app
+
+    def click_create_group(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("групи").click()
+
+    def create(self, group):
+        wd = self.app.wd
+        self.click_create_group()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        # open group page
+        wd.find_element_by_link_text("group page").click()
+
+    def enter_address(self, group):
+        wd = self.app.wd
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(group.address2)
+        wd.find_element_by_name("phone2").click()
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(group.phone2)
+        wd.find_element_by_name("notes").click()
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(group.notes)
+
+    def click_enter_btn(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def enter_phone_numbers(self, group):
+        wd = self.app.wd
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(group.home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(group.mobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(group.work)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(group.fax)
+
+    def enter_emails(self, group):
+        wd = self.app.wd
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(group.email)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(group.email2)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(group.email3)
+
+    def enter_value_dropdown(self):
+        wd = self.app.wd
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[6]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[6]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[2]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[2]").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys("1988")
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[7]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[7]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[6]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[6]").click()
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys("1988")
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[5]//option[7]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[5]//option[7]").click()
+
+    def add_new_contact(self, group):
+        wd = self.app.wd
+        wd.find_element_by_link_text("добави нов").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(group.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").send_keys(group.middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(group.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(group.nickname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(group.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(group.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(group.address)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(group.homepage)
