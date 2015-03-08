59d00c898e5226562db8842a4f416c6addd98684
Властелин Вселенной <wartooth@tut.by>
2015-03-08 19:30:27 +0300

    add packages for different sphere of response

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from  parameters import *
-from application import Application
+from  model.parameters import *
+from fixture.application import Application
 
 
 @pytest.fixture
--- tests/test_add_group2.py
+++ tests/test_add_group2.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from parameters import Group
-from application import Application
+from model.parameters import Group
+from fixture.application import Application
 
 
 @pytest.fixture
