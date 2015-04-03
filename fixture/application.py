bac382ef847104dfd2d09613cde485a3a3f2565f
Властелин Вселенной <wartooth@tut.by>
2015-04-03 23:49:51 +0300

    set browser and url

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,5 @@
 __author__ = 'Властелин Вселенной'
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -7,15 +7,23 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser=="chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognize browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url=base_url
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
+        wd.get(self.base_url)
 
     def destroy(self):
          self.wd.quit()
